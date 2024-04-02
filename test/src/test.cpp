62192a5657dcc1e1f02a0083b163e7c5058fd54b
Florian Becker <info@florianbecker.eu>
2024-04-02 17:48:06 +0200

    modified test

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -1,8 +1,18 @@
+#include "lib.h"
+#include "utils.h"
+
+#include <filesystem>
 #include <gtest/gtest.h>
 
-#include "library.h"
+TEST(IFC4, LoadFile) {
+    OpenBimRL::Engine::Utils::setSilent(true);
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-    EXPECT_EQ(hello(), 0);
+    const auto init = initIfc(std::filesystem::path(RESOURCES_DIR).append("test.ifc").c_str());
+
+    ASSERT_TRUE(init);
+}
+
+TEST(IFC4, DetermineIFCVersion) {
+    ASSERT_TRUE(OpenBimRL::Engine::Utils::isIFC4());
+    ASSERT_FALSE(OpenBimRL::Engine::Utils::isIFC2x3());
 }
