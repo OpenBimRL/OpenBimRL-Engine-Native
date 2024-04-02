96110cb3f2c6e82c1f49b96a4f22dadb978554e7
Florian Becker <info@florianbecker.eu>
2024-04-02 14:49:25 +0200

    changed file structure

--- library.cpp
+++ library.cpp
@@ -8,7 +8,7 @@ int hello() {
     std::cout << "Looking for file in path: " << std::filesystem::current_path() << std::endl;
 
     // Parse the IFC file provided in fileName
-    auto file = new IfcParse::IfcFile("../test.ifc");
+    auto file = new IfcParse::IfcFile("../test/resources/test.ifc");
     if (!file->good())
     {
         std::cerr << "Unable to parse .ifc file" << std::endl;
