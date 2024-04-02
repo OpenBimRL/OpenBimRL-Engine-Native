7bb724cfd14a113ffb4f7267e3445dcf55d55422
Florian Becker <info@florianbecker.eu>
2024-04-02 11:36:01 +0200

    let there be light

---
+++ library.cpp
@@ -0,0 +1,19 @@
+#include "library.h"
+
+#include <iostream>
+#include <filesystem>
+#include <ifcparse/IfcFile.h>
+
+int hello() {
+    std::cout << "Looking for file in path: " << std::filesystem::current_path() << std::endl;
+
+    // Parse the IFC file provided in fileName
+    auto file = new IfcParse::IfcFile("../test.ifc");
+    if (!file->good())
+    {
+        std::cerr << "Unable to parse .ifc file" << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
---
+++ test/test.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+
+#include "../library.h"
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+    EXPECT_EQ(hello(), 0);
+}
