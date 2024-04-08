d11d6f8bfda0b6839d67eaba15c4f421cea7f0bd
Florian Becker <info@florianbecker.eu>
2024-04-08 18:52:09 +0000

    added json lib and IfcData typedef

--- include/types/ifc_typedefs.h
+++ include/types/ifc_typedefs.h
@@ -5,5 +5,13 @@
 
 namespace OpenBimRL::Engine::Types::IFC {
     typedef IfcUtil::IfcBaseClass *IfcObjectPointer;
+    typedef std::map<std::string, std::string> property;
+    typedef std::map<std::string, property> property_sets;
+    typedef struct {
+        std::string name;
+        std::string type;
+        std::string GUID;
+        property_sets propertySets;
+    } IfcData;
 }
 #endif // OPENBIMRL_NATIVE_IFC_TYPEDEFS_H
