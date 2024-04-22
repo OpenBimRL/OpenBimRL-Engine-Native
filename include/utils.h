8928ad9ad2245ec169677c33ec3e757e87fef43a
Florian <Florian.Becker-f86@ruhr-uni-bochum.de>
2024-04-22 13:45:07 +0000

    resolved issue with schema

--- include/utils.h
+++ include/utils.h
@@ -6,33 +6,15 @@
 #include <iostream>
 #include <fstream>
 
-#include <TColgp_Array2OfPnt.hxx>
-#include <TColgp_Array1OfPnt.hxx>
-#include <TColStd_Array1OfReal.hxx>
-#include <TColStd_Array1OfInteger.hxx>
-
-#include <Geom_BSplineSurface.hxx>
-
-#include <BRepBuilderAPI_MakeFace.hxx>
-#include <BRepBuilderAPI_NurbsConvert.hxx>
-
-#include <BRepPrimAPI_MakeBox.hxx>
-#include <BRepPrimAPI_MakeSphere.hxx>
-
-#include <BRepAlgoAPI_Cut.hxx>
-
-#include <Standard_Version.hxx>
-
 #include <ifcparse/Ifc4.h>
-#define IfcSchema (Ifc4)
+#define IfcSchema Ifc4
 
-#define SCHEMA_VERSIONS=(2x3)(4)
-#define SCHEMA_SEQ = (2x3)(4)
+#define SCHEMA_VERSIONS (2x3)(4)
+#define SCHEMA_SEQ (2x3)(4)
 
 #include <ifcparse/IfcBaseClass.h>
 #include <ifcparse/IfcHierarchyHelper.h>
 #include <ifcgeom/IfcGeom.h>
-#include <ifcgeom_schema_agnostic/empty.h>
 
 
 namespace OpenBimRL::Engine::Utils {
