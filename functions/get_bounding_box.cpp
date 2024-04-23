79eb0027eba739d49afac2225c00878e21c7fa6a
Florian Becker <info@florianbecker.eu>
2024-04-23 22:47:42 +0200

    added getBoundingBox function

--- functions/get_bounding_box.cpp
+++ functions/get_bounding_box.cpp
@@ -1,33 +1,50 @@
+#include <ifcparse/Ifc4.h>
+
+#include <BRepBndLib.hxx>
+
 #include "functions.h"
 #include "utils.h"
 
-#include <ifcparse/Ifc4.h>
 
 [[maybe_unused]] void getBoundingBox(void) {
   using namespace OpenBimRL::Engine;
-  const auto ifcObject = (Types::IFC::IfcObjectPointer)Functions::getInputPointer(0);
+  const auto ifcObject =
+      (Types::IFC::IfcObjectPointer)Functions::getInputPointer(0);
   if (!ifcObject) return;
 
   if (Utils::isIFC4()) {
     const auto spatial = ifcObject->as<Ifc4::IfcSpatialStructureElement>();
+
     if (!spatial) return;
+    const auto representation = spatial->Representation();
 
-    // const auto geometry = OpenBimRL::Engine::Utils::getKernel()->convert(spatial);
-/*    const auto representationDefinition = spatial->Representation();
-    const auto representations = representationDefinition->Representations();
+    IfcGeom::IteratorSettings settings;
+    settings.set(IfcGeom::IteratorSettings::USE_WORLD_COORDS, true);
+    settings.set(IfcGeom::IteratorSettings::WELD_VERTICES, false);
+    settings.set(IfcGeom::IteratorSettings::DISABLE_TRIANGULATION, false);
+    settings.set(IfcGeom::IteratorSettings::APPLY_DEFAULT_MATERIALS, true);
+    settings.set(IfcGeom::IteratorSettings::NO_NORMALS, true);
 
-    if (!representations->size()) return;
+    const auto representation_list = representation->Representations();
+    const auto some_representation = *(representation_list->begin());
 
-    for (const auto representation : *representations) {
-      const auto model = representation->as<Ifc4::IfcShapeRepresentation>();
-      if (!model) continue;
+    // copying this is not optimal but there are what? 60 doubles? I think
+    // that's manageable
+    const auto element =
+        Utils::create_shape(settings, spatial, some_representation);
 
-      const auto items = model->Items();
+    Bnd_Box bbox;
 
-      for (const auto item : *(items)) {
-        const auto solid = item->as<Ifc4::IfcSweptAreaSolid>();
-        const auto test = nullptr;
-      }
-    }*/
+    BRepBndLib::AddOptimal(element, bbox);
+    const auto corner_min = bbox.CornerMin(), corner_max = bbox.CornerMax();
+
+    const auto mem_size = 6 * sizeof(Standard_Real);
+    auto buffer = Functions::setOutputArray(0, mem_size);
+    const Standard_Real values[6] = {
+        corner_min.X(), corner_min.Y(), corner_min.Z(),
+        corner_max.X(), corner_max.Y(), corner_max.Z(),
+    };
+
+    std::memcpy(buffer, values, mem_size);
   }
 }
