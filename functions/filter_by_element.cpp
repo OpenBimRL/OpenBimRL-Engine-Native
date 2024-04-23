05e69cdcfec7698fe1818c79ceef0e612218cbf7
Florian Becker <info@florianbecker.eu>
2024-04-23 22:46:40 +0200

    #format

--- functions/filter_by_element.cpp
+++ functions/filter_by_element.cpp
@@ -2,28 +2,30 @@
 #include "utils.h"
 
 [[maybe_unused]] void filterByElement(void) {
-    using namespace OpenBimRL::Engine::Types;
-    JNA::String input = OpenBimRL::Engine::Functions::getInputString(0); // get input from graph
+  using namespace OpenBimRL::Engine;
+  Types::JNA::String input =
+      Functions::getInputString(0);  // get input from graph
 
-    // check for null
-    if (input == nullptr) {
-        OpenBimRL::Engine::Functions::setOutputPointer(0, nullptr);
-        return;
-    }
+  // check for null
+  if (input == nullptr) {
+    Functions::setOutputPointer(0, nullptr);
+    return;
+  }
 
-    const auto ifcClass = std::string(input); // create c++ string from char*
+  const auto ifcClass = std::string(input);  // create c++ string from char*
 
-    IfcParse::IfcFile *file = OpenBimRL::Engine::Utils::getCurrentFile(); // get active file
+  const auto file = Utils::getCurrentFile();  // get active file
 
-    const auto instances = file->instances_by_type(ifcClass); // get instances by ifc class excluding subtypes
-    const auto size = instances->size();
-    if (!size) {
-        OpenBimRL::Engine::Functions::setOutputPointer(0, nullptr);
-        return;
-    }
+  const auto instances = file->instances_by_type(
+      ifcClass);  // get instances by ifc class excluding subtypes
+  const auto size = instances->size();
+  if (!size) {
+    Functions::setOutputPointer(0, nullptr);
+    return;
+  }
 
-    const auto buffer = (OpenBimRL::Engine::Types::IFC::IfcObjectPointer *) OpenBimRL::Engine::Functions::setOutputArray(
-            0, size);
+  const auto buffer =
+      (Types::IFC::IfcObjectPointer *)Functions::setOutputArray(0, size * sizeof(void *));
 
-    std::copy(instances->begin(), instances->end(), buffer);
+  std::copy(instances->begin(), instances->end(), buffer);
 }
