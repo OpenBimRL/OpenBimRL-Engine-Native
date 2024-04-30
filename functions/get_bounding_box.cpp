3e70854eca672aedef1e7115e8ff535d01150bd6
Florian Becker <info@florianbecker.eu>
2024-04-30 15:03:46 +0200

    added comments to get_bounding_box.cpp

--- functions/get_bounding_box.cpp
+++ functions/get_bounding_box.cpp
@@ -13,21 +13,22 @@
       (Types::IFC::IfcObjectPointer)Functions::getInputPointer(0);
   if (!ifcObject) return;
 
-  if (Utils::isIFC4()) {
+  if (Utils::isIFC4()) { // currently only works with IFC4
     const auto spatial = ifcObject->as<Ifc4::IfcSpatialStructureElement>();
+    // check if element is of type IfcSpatialStructureElement (IfcSpace, IfcSite, ...) could probably expanded to any IfcElement with geometry
 
     if (!spatial) return;
     const auto representation = spatial->Representation();
 
-    IfcGeom::IteratorSettings settings;
+    IfcGeom::IteratorSettings settings; // configure geometry generator settings
     settings.set(IfcGeom::IteratorSettings::USE_WORLD_COORDS, true);
     settings.set(IfcGeom::IteratorSettings::WELD_VERTICES, false);
     settings.set(IfcGeom::IteratorSettings::DISABLE_TRIANGULATION, false);
     settings.set(IfcGeom::IteratorSettings::APPLY_DEFAULT_MATERIALS, true);
-    settings.set(IfcGeom::IteratorSettings::NO_NORMALS, true);
+    settings.set(IfcGeom::IteratorSettings::NO_NORMALS, true); // we don't need normals for simple point geometry
 
     const auto representation_list = representation->Representations();
-    const auto some_representation = *(representation_list->begin());
+    const auto some_representation = *(representation_list->begin()); // take the first (any most of the times the only) representation
 
     // copying this is not optimal but there are what? 60 doubles? I think
     // that's manageable
@@ -36,16 +37,18 @@
 
     Bnd_Box bbox;
 
-    BRepBndLib::AddOptimal(element, bbox);
+    BRepBndLib::AddOptimal(element, bbox); // automatically expands the box to fit given geometry by adding points util every point is inside the box
     const auto corner_min = bbox.CornerMin(), corner_max = bbox.CornerMax();
 
+    // prepare buffer from JVM
     const auto mem_size = 6 * sizeof(Standard_Real);
-    auto buffer = Functions::setOutputArray(0, mem_size);
-    const Standard_Real values[6] = {
+    auto buffer = Functions::setOutputArray(0, mem_size); // JVM collects garbage automatically
+    const Standard_Real values[6] = { // construct array
         corner_min.X(), corner_min.Y(), corner_min.Z(),
         corner_max.X(), corner_max.Y(), corner_max.Z(),
     };
 
+    // copy memory from stack into JVM provided memory
     std::memcpy(buffer, values, mem_size);
   }
 }
