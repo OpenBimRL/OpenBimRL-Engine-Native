cbc99b7fa185d89ceed0b8e28320e03518812e21
Florian Becker <info@florianbecker.eu>
2024-07-22 21:13:14 +0200

    added calculate_building_bounds.cpp

--- include/functions.h
+++ include/functions.h
@@ -7,6 +7,7 @@ extern "C" {
 [[maybe_unused]] void filterByElement(void);
 [[maybe_unused]] void filterByGUID(void);
 [[maybe_unused]] void getBoundingBox(void);
+[[maybe_unused]] void calculatingBuildingBounds(void);
 }
 
 #endif //OPENBIMRL_NATIVE_FUNCTIONS_H
